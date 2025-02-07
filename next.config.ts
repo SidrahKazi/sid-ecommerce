5c2f1a3d187c887291cc0ec9beb183f8f027891d
sid-90 <161483276+SidrahKazi@users.noreply.github.com>
2025-02-08 00:18:48 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ['images.pexels.com'],
+  eslint:{
+    ignoreDuringBuils: true,
   },
 };
 
